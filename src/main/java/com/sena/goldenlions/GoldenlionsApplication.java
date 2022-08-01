4bd4142bbe9d0c6f6449a477009021b714fd12e4
Yuliani Espitia <yuliani.espitia@e-capture.co>
2022-08-01 11:48:01 -0500

    Primer commit

---
+++ src/main/java/com/sena/goldenlions/GoldenlionsApplication.java
@@ -0,0 +1,13 @@
+package com.sena.goldenlions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenlionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenlionsApplication.class, args);
+	}
+
+}
